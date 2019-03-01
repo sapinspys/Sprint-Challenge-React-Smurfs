338b4fea4187a3dbf196debb76b5a88033056822
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-01 12:22:53 -0500

    Cards styled and delete smurf button added, no functionality

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -2,14 +2,32 @@ import React from 'react';
 
 import { Card, CardTitle, CardText, Button } from 'reactstrap';
 
+// Inline Styles
+const cardStyles = {
+  width: '30%',
+  minWidth: '280px',
+  marginBottom: '20px', 
+  padding: '15px'
+}
+
 const Smurf = props => {
   return (
-    <Card className="Smurf">
-      <CardTitle>{props.name}</CardTitle>
+    <Card inverse color='info' style={cardStyles}>
+      <CardTitle style={{
+                fontSize:'1.4rem', 
+                borderBottom: '1px solid white'
+                }}>
+                    {props.name}
+            </CardTitle>
       <CardText>
         <strong>{props.height} tall</strong>
       </CardText>
       <CardText>{props.age} smurf years old</CardText>
+      <Button onClick={() => console.log('testing')}
+          size='sm'
+          color='warning'>
+              Delete Smurf
+      </Button>
     </Card>
   );
 };
