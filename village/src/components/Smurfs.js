6581b6fbdb43ae39dbb4b76926ea92ab4878b5c5
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-01 12:31:16 -0500

    Finalized smurf card styling

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
 
+import styled from 'styled-components';
+
 import Smurf from './Smurf';
+
+// Styled Components
+const SmurfContainer = styled.div`
+    margin-top: 20px;
+`;
+
+const WrapperContainer = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+    margin-top: 30px;
+`;
 
 class Smurfs extends Component {
   render() {
     return (
-      <div className="Smurfs">
+      <SmurfContainer>
         <h1>Smurf Village</h1>
-        <ul>
-          {this.props.smurfs.map(smurf => {
-            return (
-              <Smurf
-                name={smurf.name}
-                id={smurf.id}
-                age={smurf.age}
-                height={smurf.height}
-                key={smurf.id}
-              />
-            );
-          })}
-        </ul>
-      </div>
+        <WrapperContainer>
+            {this.props.smurfs.map(smurf => {
+              return (
+                <Smurf
+                  name={smurf.name}
+                  id={smurf.id}
+                  age={smurf.age}
+                  height={smurf.height}
+                  key={smurf.id} />);
+            })}
+        </WrapperContainer>
+      </SmurfContainer>
     );
   }
 }
